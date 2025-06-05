593c605850ea5b4921bdc622f13ab27438e630ca
RISHIKANTH-S <suggalarishikanth@gmail.com>
2025-06-04 21:45:58 -0500

    Secure SMTP credentials using environment variables

--- src/main/java/com/example/TimeTracker/TimeTrackerApplication.java
+++ src/main/java/com/example/TimeTracker/TimeTrackerApplication.java
@@ -3,10 +3,12 @@ package com.example.TimeTracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 @EnableScheduling
 public class TimeTrackerApplication {
+	Dotenv dotenv = Dotenv.load();
 	public static void main(String[] args) {
 		SpringApplication.run(TimeTrackerApplication.class, args);
 	}
